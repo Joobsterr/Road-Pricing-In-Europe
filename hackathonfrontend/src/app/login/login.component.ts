d5d14a3fca8592f799f0c736f9a4d45504768650
Gino <ginovandegraaf@hotmail.com>
2022-08-31 09:59:39 +0200

    :wrench: Redirection after correct login

--- hackathonfrontend/src/app/login/login.component.ts
+++ hackathonfrontend/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ɵclearResolutionOfComponentResourcesQueue } from '@angular/core';
+import {Router} from "@angular/router"
+
 
 @Component({
   selector: 'app-login',
@@ -8,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   username:string;
   password:string;
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
@@ -18,6 +20,13 @@ export class LoginComponent implements OnInit {
     if(this.username=="admin" && this.password=="password")
     {
       //complete login.
+      console.log("Login");
+      this.router.navigate(['/dashboard']);
+
+    }
+    else
+    {
+      console.log("Login Failed");
     }
   }
 }
