4f1825cdf011aae8b2e9ae3ba3d001ae9bd78f90
Gino <ginovandegraaf@hotmail.com>
2022-10-17 13:15:56 +0200

    administrator

---
+++ RoadPricing .NET/Road Pricing/AdministrativeService/Service/IAdministartionService.cs
@@ -0,0 +1,14 @@
+﻿using AdministrativeService.Models;
+
+namespace AdministrativeService.Service
+{
+    public interface IAdministartionService
+    {
+        Task<bool> CreateNewPrice();
+        Task<bool> UpdatePrice();
+        Task<bool> DeletePrice();
+        Task<List<AdministrationPrices>> GetAllPrices();
+        Task<AdministrationPrices> GetPrice();
+
+    }
+}
