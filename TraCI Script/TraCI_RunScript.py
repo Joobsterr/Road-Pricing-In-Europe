73074a6604bbbadb0dd0306fa8e596ca595010a5
Nardo Janssen <nardo.janssen@student.fontys.nl>
2022-10-20 12:41:36 +0200

    Added some comments to the TraCI file

--- TraCI Script/TraCI_RunScript.py
+++ TraCI Script/TraCI_RunScript.py
@@ -9,7 +9,8 @@ import traci.constants as tc
 import pika#chu
 import sys
 
-# Methods
+### Methods
+# RabbitMQ data producer method
 def sendData(carId, lat: str, lon: str):
     exchangeKey = 'sumoData'
     routingKey = 'dataService'
@@ -23,28 +24,33 @@ def sendData(carId, lat: str, lon: str):
     channel.basic_publish(exchange=exchangeKey, routing_key=routingKey, body=message)
     connection.close()
 
-# Main
+### Main
+# Checks if SUMO is toegankelijk via de environment variables
 if 'SUMO_HOME' in os.environ:
     tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
     sys.path.append(tools)
 else:
     sys.exit("please declare environment variable 'SUMO_HOME'")
 
+# Specificieerd de locatie van SUMO en het configuratie bestand
 sumoExecutable = "D:/Program Files (x86)/Eclipse/Sumo/bin/SUMO-gui.exe"
 sumoConfigFile = "D:/GIT-projects/S6/Road-Pricing-In-Europe/TraCI Script/minimap.sumo.cfg"
 simulationDuration = 1000
 
 sumoCmd = [sumoExecutable, "-c", sumoConfigFile]
 
-
+# Start het TraCI script
 traci.start(sumoCmd)
 step = 0
 while step < simulationDuration:
     traci.simulationStep()
+    # Als de stap een meervoud is van 10, dan stuurt hij de data op
     if step%10 == 0:
         for carId in traci.vehicle.getIDList():
+            # Haalt de specifieke data van het voertuig op die gewenst is
             x, y = traci.vehicle.getPosition(carId)
             lon, lat = traci.simulation.convertGeo(x, y)
+            # Stuurt de data via RabbitMQ naar de data processing service
             sendData(str(carId), str(lat), str(lon))
     step += 1
 
