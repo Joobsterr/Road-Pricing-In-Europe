047df72fbafc7dca6af31fdd331b0596133c5962
odranJ <nardo.janssen@student.fontys.nl>
2022-12-06 11:10:44 +0100

    Changed prices to be a bit lower and modified small frontend bit for demo

--- RoadPricing .NET/Road Pricing/BillingService/Services/BillService.cs
+++ RoadPricing .NET/Road Pricing/BillingService/Services/BillService.cs
@@ -76,9 +76,9 @@ namespace BillingService.Services
             switch (carType)
             {
                 case "DEFAULT_VEHTYPE":
-                    return 0.50;
+                    return 0.05;
                 default:
-                    return 0;
+                    return 0.01;
             }
         }
 
@@ -87,37 +87,37 @@ namespace BillingService.Services
             switch (maxSpeedLane)
             {
                 case < 7:  //20kmh
-                    return 1.00;
+                    return 0.10;
 
                 case < 9:  //30kmh
-                    return 1.25;
+                    return 0.15;
 
                 case < 15: //50kmh
-                    return 1.75;
+                    return 0.25;
 
                 case < 18: //60kmh
-                    return 2.00;
+                    return 0.30;
 
                 case < 20: //70kmh
-                    return 2.25;
+                    return 0.35;
 
                 case < 23: //80kmh
-                    return 2.50;
+                    return 0.40;
 
                 case < 29: //100kmh
-                    return 3.00;
+                    return 0.50;
 
                 case < 34: //120kmh
-                    return 4.00;
+                    return 0.60;
 
                 case < 37: //130kmh
-                    return 5.00;
+                    return 0.65;
 
                 case > 36:
-                    return 5.00;
+                    return 1.00;
 
                 default:
-                    return 0;
+                    return 0.01;
             }
         }
 
@@ -126,11 +126,11 @@ namespace BillingService.Services
             switch (fuelType)
             {
                 case "petrol":
-                    return 15;
+                    return 0.05;
                 case "electric":
-                    return 10;
+                    return 0.02;
                 default:
-                    return 0;
+                    return 0.01;
             }
         }
 
@@ -144,32 +144,32 @@ namespace BillingService.Services
                 case 3:
                 case 4:
                 case 5:
-                    return 0.50;
+                    return 0.10;
                 case 6:
                 case 7:
                 case 8:
                 case 9:
-                    return 2.00;
+                    return 0.50;
                 case 10:
                 case 11:
                 case 12:
                 case 13:
                 case 14:
                 case 15:
-                    return 1.00;
+                    return 0.20;
                 case 16:
                 case 17:
                 case 18:
                 case 19:
-                    return 1.50;
+                    return 0.40;
                 case 20:
                 case 21:
                 case 22:
                 case 23:
                 case 24:
-                    return 1.00;
+                    return 0.30;
                 default:
-                    return 0;
+                    return 0.01;
             }
         }
     }
